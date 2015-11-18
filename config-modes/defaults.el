c2974b37394bfccb4e9d4e15232097e607ab9fb8
Valera Rozuvan <valera.rozuvan@gmail.com>
2015-11-18 18:43:17 +0200

    Adding sane auto indent rules for most modes.

--- config-modes/defaults.el
+++ config-modes/defaults.el
@@ -23,6 +23,9 @@
 )
 (dolist (
     hook '(
+        c++-mode-hook
+        c-mode-hook
+
         emacs-lisp-mode-hook
         text-mode-hook
         fundamental-mode-hook
@@ -51,6 +54,70 @@
     (add-hook hook 'set-newline-for-return)
     (add-hook hook 'reset-indent-function)
 )
+
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Taken from:
+;;
+;;   How to get auto indent (not smart indent) in emacs in all modes
+;;   http://stackoverflow.com/questions/18945380/
+;;
+
+(defun plain-tab ()
+  (interactive)
+  (insert (make-string tab-width ?\ )))
+(defun plain-ret ()
+  (interactive)
+  (looking-back "^\\( +\\).*")
+  (newline)
+  (insert (match-string 1)))
+(defun plain-del ()
+  (interactive)
+  (backward-delete-char
+   (if (looking-back (format " \\{%d\\}" tab-width)) tab-width 1)))
+(defvar all-the-mode-maps
+  '(
+    c++-mode-map
+    c-mode-map
+
+    emacs-lisp-mode-map
+    text-mode-map
+    ;; fundamental-mode-map
+
+    ;; python-mode-map
+
+    js-mode-map
+    ;; javascript-mode-map
+    js2-mode-map
+    ;; coffee-mode-map
+
+    typescript-mode-map
+
+    ;; sass-mode-map
+    less-css-mode-map
+    css-mode-map
+
+    yaml-mode-map
+
+    ;; markdown-mode-map
+
+    ;; shell-script-mode-map
+    ;; sh-mode-map
+  )
+)
+(eval `(mapc
+        (lambda(map)
+          (define-key map [tab] 'plain-tab)
+          (define-key map [return] 'plain-ret)
+          (define-key map [backspace] 'plain-del)
+          (define-key map "{" (lambda()(interactive)(insert "{")))
+          (define-key map "}" (lambda()(interactive)(insert "}"))))
+        (list ,@all-the-mode-maps)))
 
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
